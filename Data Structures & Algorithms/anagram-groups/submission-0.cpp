4f20e2409357b154aeded2433094dee1097936be
Saurabh Patel <87745383+setbit1@users.noreply.github.com>
2026-09-25 02:37:48 +0530

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.cpp
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    vector<int> createValue(string s)
-    {
-        vector<int> val(26, 0);
-        for(auto x : s)
-            val[x-'a']++;
-        return val;
-    }
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>, vector<string>> maps;
+        map<vector<int>, vector<string>> mp;
+        for(int i=0; i<strs.size(); i++){
+            vector<int> counter(26, 0);
+            for(auto c : strs[i]){
+                counter[c-'a']++;
+            }
+            mp[counter].push_back(strs[i]);
+        }
         vector<vector<string>> ans;
-        for(auto s : strs)
-            maps[createValue(s)].push_back(s);
-            
-        for(auto const& [key, value] : maps)
-            ans.push_back(value);
+        for(auto [v, s] : mp){
+            ans.push_back(s);
+        }
         return ans;
     }
 };
